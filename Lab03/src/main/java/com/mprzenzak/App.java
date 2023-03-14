ee45699980cde702bb93211b91d64debba9f4f8e
Mikołaj Przenzak <mikprz066_dpp_2023@e-science.pl>
2023-03-15 00:48:14 +0100

    Pełna funkcjonalność wyświetlania pytań i sprawdzania odpowiedzi

--- Lab03/src/main/java/com/mprzenzak/App.java
+++ Lab03/src/main/java/com/mprzenzak/App.java
@@ -1,86 +1,112 @@
 package com.mprzenzak;
 
+import com.google.gson.Gson;
+
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 public class App {
     private JFrame frame;
-    private JPanel panelFiles;
-    private JTable table;
-    private DefaultTableModel model;
-    private JButton btnSelect;
+    private JButton showQuestionButton;
+    private JButton submitButton;
     private JLabel question = new JLabel();
     private JLabel check = new JLabel();
     private JTextField answerField = new JTextField();
+
+    private String json;
+
     public App() {
         initComponents();
     }
 
-    private void initComponents() throws IOException {
+    private void initComponents() {
         frame = new JFrame("App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(1280, 720));
+        frame.setSize(new Dimension(600, 400));
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
 
-        panelFiles = new JPanel(new GridLayout(0, 1));
-        JScrollPane scrollPane = new JScrollPane(panelFiles);
-        frame.add(scrollPane, BorderLayout.CENTER);
-        //JLabel question = new JLabel("Type the song author name.");
-        btnSelect = new JButton("Submit");
-        btnSelect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout());
+        panel.setLayout(new FlowLayout());
+        frame.add(panel, BorderLayout.CENTER);
+        boolean fileLoadedFromCache = false;
 
+        showQuestionButton = new JButton("Show question");
+        submitButton = new JButton("Submit answer");
+        showQuestionButton.addActionListener(e -> {
+            try {
+                getRandomQuestion();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
 
-        frame.add(question, BorderLayout.NORTH);
-        frame.add(answerField, BorderLayout.NORTH);
-        frame.add(btnSelect, BorderLayout.NORTH);
-        frame.add(check, BorderLayout.NORTH);
+        submitButton.addActionListener(e -> submitAnswer());
+
+        answerField.setSize(100, 20);
+        answerField.setText("Enter your answer here");
+
+        panel.add(showQuestionButton);
+        panel.add(question);
+        panel.add(answerField);
+        panel.add(submitButton);
+        panel.add(check);
 
         frame.setVisible(true);
-        getRandomQuestion();
     }
 
     public void getRandomQuestion() throws IOException {
-        String apiUrl = "https://musicbrainz.org/ws/2/recording?limit=1&offset=" + (int) (Math.random() * 10000);
+        URL apiUrl = new URL("https://musicbrainz.org/ws/2/release/?query=*&limit=1&offset=" + (int) (Math.random() * 1000));
+        json = sendGETRequest(apiUrl);
+        String songTitle = getTitleFromJson(json);
+        String songArtist = getArtistFromJson(json);
+        question.setText("Hint: " + songArtist + "Enter the artist of the song \"" + songTitle + "\": ");
+}
 
-        HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setRequestProperty("Authorization", "Bearer YOUR_API_TOKEN_HERE");
-
-        Scanner scanner = new Scanner(connection.getInputStream());
-        scanner.useDelimiter("\\A");
-        String responseBody = scanner.hasNext() ? scanner.next() : "";
-        scanner.close();
-
-        JSONObject responseJson = new JSONObject(responseBody);
-        JSONArray recordings = responseJson.getJSONArray("recordings");
-        JSONObject recording = recordings.getJSONObject(0);
-        String artistName = recording.getJSONArray("artist-credit").getJSONObject(0).getJSONObject("artist").getString("name");
-        String songTitle = recording.getString("title");
-        String releaseDate = recording.getJSONArray("release-groups").getJSONObject(0).getString("first-release-date");
-
-        this.question.setText("Enter the artist of the song \"" + songTitle + "\": ");
-        String userInput = this.answerField.getText();
-
-        System.out.println("The song \"" + songTitle + "\" was released by " + artistName + " on " + releaseDate + ".");
-        if (userInput.equalsIgnoreCase(artistName)) {
-            this.check.setText("You are correct!");
+    private void submitAnswer() {
+        String answer = answerField.getText();
+        String artistName = getArtistFromJson(json);
+        if (answer.equals(artistName)) {
+            check.setText("Correct!");
         } else {
-            this.check.setText("You are incorrect. The correct answer is " + artistName + ".");
+            check.setText("Incorrect! The correct answer is: " + artistName + ".");
         }
     }
-    public static void main(String[] args) {
+
+    private static String getTitleFromJson(String json) {
+        Gson gson = new Gson();
+        JsonData jsonData = gson.fromJson(json, JsonData.class);
+        return jsonData.getReleases().get(0).getTitle();
+    }
+
+    private static String getArtistFromJson(String json) {
+        Gson gson = new Gson();
+        JsonData jsonData = gson.fromJson(json, JsonData.class);
+        return jsonData.getReleases().get(0).getArtistCredit().get(0).getName();
+    }
+
+    private static String sendGETRequest(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept", "application/json");
+
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
         new App();
     }
 }
---
+++ Lab03/src/main/java/com/mprzenzak/JsonData.java
@@ -0,0 +1,288 @@
+package com.mprzenzak;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+class JsonData {
+    private String created;
+    private int count;
+    private int offset;
+    private List<Release> releases;
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public List<Release> getReleases() {
+        return releases;
+    }
+
+    public void setReleases(List<Release> releases) {
+        this.releases = releases;
+    }
+}
+
+class Release {
+    private String id;
+    private int score;
+    @SerializedName("status-id")
+    private String statusId;
+    private int count;
+    private String title;
+    private String status;
+    @SerializedName("artist-credit")
+    private List<ArtistCredit> artistCredit;
+    private ReleaseGroup releaseGroup;
+    private List<LabelInfo> labelInfo;
+    private int trackCount;
+    private List<Media> media;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<ArtistCredit> getArtistCredit() {
+        return artistCredit;
+    }
+
+    public void setArtistCredit(List<ArtistCredit> artistCredit) {
+        this.artistCredit = artistCredit;
+    }
+
+    public ReleaseGroup getReleaseGroup() {
+        return releaseGroup;
+    }
+
+    public void setReleaseGroup(ReleaseGroup releaseGroup) {
+        this.releaseGroup = releaseGroup;
+    }
+
+    public List<LabelInfo> getLabelInfo() {
+        return labelInfo;
+    }
+
+    public void setLabelInfo(List<LabelInfo> labelInfo) {
+        this.labelInfo = labelInfo;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public List<Media> getMedia() {
+        return media;
+    }
+
+    public void setMedia(List<Media> media) {
+        this.media = media;
+    }
+}
+
+class ArtistCredit {
+    private String name;
+    private Artist artist;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+}
+
+class Artist {
+    private String id;
+    private String name;
+    @SerializedName("sort-name")
+    private String sortName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSortName() {
+        return sortName;
+    }
+
+    public void setSortName(String sortName) {
+        this.sortName = sortName;
+    }
+}
+
+class ReleaseGroup {
+    private String id;
+    private String typeId;
+    private String primaryTypeId;
+    private String title;
+    private String primaryType;
+    private List<String> secondaryTypes;
+    private List<String> secondaryTypeIds;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getPrimaryTypeId() {
+        return primaryTypeId;
+    }
+
+    public void setPrimaryTypeId(String primaryTypeId) {
+        this.primaryTypeId = primaryTypeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrimaryType() {
+        return primaryType;
+    }
+
+    public void setPrimaryType(String primaryType) {
+        this.primaryType = primaryType;
+    }
+
+    public List<String> getSecondaryTypes() {
+        return secondaryTypes;
+    }
+
+    public void setSecondaryTypes(List<String> secondaryTypes) {
+        this.secondaryTypes = secondaryTypes;
+    }
+
+    public List<String> getSecondaryTypeIds() {
+        return secondaryTypeIds;
+    }
+
+    public void setSecondaryTypeIds(List<String> secondaryTypeIds) {
+        this.secondaryTypeIds = secondaryTypeIds;
+    }
+}
+
+class LabelInfo {
+    private String catalogNumber;
+    private Label label;
+
+    // getters and setters
+}
+
+class Label {
+    private String id;
+    private String name;
+
+    // getters and setters
+}
+
+class Media {
+    private String format;
+    private int discCount;
+    private int trackCount;
+
+    // getters and setters
+}
